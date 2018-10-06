6691cd475fe0eb01626eca11f98246b7fa745c29
Adriana Kasum <kasumi781.ak@gmail.com>
2018-10-06 13:05:14 +0200

    commit

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,7 +1,13 @@
 package pl.sda;
 
+
 public class Main {
     public static void main(String ... args) {
-        System.out.println("test");
+        System.out.println(args.length);
+        System.out.println(getMsg());
+    }
+
+    private static String getMsg(){
+        return "test";
     }
 }
